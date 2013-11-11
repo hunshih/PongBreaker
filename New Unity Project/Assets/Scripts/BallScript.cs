db40c6a27be4340359f161f1252d9b220b733ee5
Shu Lin <linshu123@gmail.com>
2013-11-11 12:40:44 -0500

    initial commit

---
+++ New Unity Project/Assets/Scripts/BallScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce(5f, 0, 50f);
+		//rigidbody.velocity = 100 * Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//maintain constant speed
+		rigidbody.velocity = 35 * rigidbody.velocity.normalized;
+	}
+	
+	void onCollisionEnter (Collision col) {
+		
+		foreach (ContactPoint contact in col.contacts) {
+			if (contact.otherCollider == collider) {
+				// this is the paddle's contact point
+				float english = contact.point.x - contact.otherCollider.transform.position.x;
+				contact.thisCollider.rigidbody.AddForce(300f * english, 0, 0);
+				
+		
+			}
+		}
+		
+	}
+}
---
+++ New Unity Project/Assets/Scripts/SpawnBricks.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnBricks : MonoBehaviour {
+
+	public GameObject 		myPrefab;
+	public float			radius = 4f;
+	public List<GameObject>	spawnedObjects;
+	public int				num_objects = 0;
+	
+	// Use this for initialization
+	void Start () {
+			
+		for (float z = 0; z < 5; z++)
+        {
+            for (float x = 0; x < 5; x++)
+            {
+                //Instantiate(brick, new Vector3(3*x, 0, 2*z), Quaternion.identity);			
+            	//GameObject go = Instantiate(Resources.Load("brick"));
+				Vector3 pos = new Vector3(3f*x, 0, 2f*z);
+				spawnedObjects = new List<GameObject>();
+				GameObject go = Instantiate(myPrefab, pos, transform.rotation) as GameObject;
+				//go.transform.position.Set(3f*x, 0f, 2f*z);
+				//go.transform.position = Random.insideUnitSphere*radius;
+				
+				spawnedObjects.Add(go);
+				num_objects++;
+			}
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
