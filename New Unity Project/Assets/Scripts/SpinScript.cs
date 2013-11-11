7ccca37a616cad55e5ccab9813ad01754965cbc2
Shu Lin <linshu123@gmail.com>
2013-11-11 16:31:40 -0500

    new files

---
+++ New Unity Project/Assets/Scripts/SpinScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.R(0, 10, 0);
+		//transform.RotateAround(new Vector3( 0, 0, 0), new Vector3(0, 1, 0), 0.1f);
+		transform.Rotate(Vector3.up * Time.deltaTime * 100f);
+		
+	}
+}
